291941a21c89175c179724d6819fa5019dc2d49c
Reweb-Bruno <60668141+Reweb-Bruno@users.noreply.github.com>
2021-11-17 02:45:44 -0300

    Ajustes imagens

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -31,7 +31,24 @@ const banner = new Swiper(document.querySelector('.swiper.-banner'), {
         prevEl: document.querySelector('.swiper-button-prev.-banner'),
     },
 });
-banner.autoplay.start()
+const bannerMobile = new Swiper(document.querySelector('.swiper.-bannerMobile'), {
+    // Optional parameters
+    speed: 400,
+    effect: 'slide',
+    direction: 'horizontal',
+    loop: true,
+    // If we need pagination
+    pagination: {
+        el: document.querySelector('.swiper-pagination.-bannerMobile'),
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: document.querySelector('.swiper-button-next.-bannerMobile'),
+        prevEl: document.querySelector('.swiper-button-prev.-bannerMobile'),
+    },
+});
+banner.autoplay.start();
+bannerMobile.autoplay.start();
 const products = new Swiper(document.querySelector('.swiper.-product'), {
     speed: 400,
     effect: 'cards',
